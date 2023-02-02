eb737cec67edf3e9ffb957cc66e7900bd1d9e912
169233 <benedikta.jana@seznam.cz>
2023-02-02 23:40:08 +0100

    current time in LA

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert("time is over")
+function clockLosAngeles(){
+document.querySelector(".date").innerHTML=moment().tz("America/Los_Angeles").format("MMMM Do YYYY")
+
+
+document.querySelector(".time").innerHTML=moment().tz("America/Los_Angeles").format("HH:MM:SS a")
+}
+
+clockLosAngeles()
+setInterval(clockLosAngeles,1000)
