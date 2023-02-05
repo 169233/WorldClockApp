5f228bed69866817b5d071acf3c3e420b71b4037
169233 <benedikta.jana@seznam.cz>
2023-02-05 21:45:24 +0100

    js current time and date

--- app.js
+++ app.js
@@ -1,9 +1,55 @@
 function clockLosAngeles(){
-document.querySelector(".date").innerHTML=moment().tz("America/Los_Angeles").format("MMMM Do YYYY")
+    let elementLosAngeles=document.querySelector("#los-angeles")
+    if (elementLosAngeles){
+    let elementDate=elementLosAngeles.querySelector(".date")
+    let elementTime=document.querySelector("#time")
+    let elementAmerica=moment().tz("America/Los_Angeles")
 
-
-document.querySelector(".time").innerHTML=moment().tz("America/Los_Angeles").format("HH:MM:SS a")
+  elementDate.innerHTML=elementAmerica.format("MMMM Do YYYY")
+  elementTime.innerHTML=elementAmerica.format("HH:mm:ss a")
+    }
 }
-
-clockLosAngeles()
 setInterval(clockLosAngeles,1000)
+
+function clockOslo(){
+    let elementOslo=document.querySelector("#oslo")
+    if(elementOslo){
+    let elementDate=document.querySelector("#oslo-date")
+    let elementTime=document.querySelector("#oslo-time")
+    let elementSweden=moment().tz("Europe/Oslo")
+
+    elementDate.innerHTML=elementSweden.format("MMMM Do YYYY")
+  elementTime.innerHTML=elementSweden.format("HH:mm:ss a")
+    }
+}
+setInterval(clockOslo,1000)
+
+function clockYukon(){
+    let elementYukon=document.querySelector("#yukon")
+    if(elementYukon){
+    let elementDate=document.querySelector("#date-yukon")
+    let elementTime=document.querySelector("#time-yukon")
+    let elementCanada=moment().tz("Canada/Yukon")
+
+    elementDate.innerHTML=elementCanada.format("MMMM Do YYYY")
+  elementTime.innerHTML=elementCanada.format("HH:mm:ss a")
+    }
+}
+setInterval(clockYukon,1000)
+
+function clockTokyo(){
+    let elementTokyo=document.querySelector("#tokyo")
+    if(elementTokyo){
+    let elementDate=document.querySelector("#date-tokyo")
+    let elementTime=document.querySelector("#time-tokyo")
+    let elementJapan=moment().tz("Asia/Tokyo")
+
+    elementDate.innerHTML=elementJapan.format("MMMM Do YYYY")
+  elementTime.innerHTML=elementJapan.format("HH:mm:ss a")
+    }
+}
+setInterval(clockTokyo,1000)
+
+
+
+   
